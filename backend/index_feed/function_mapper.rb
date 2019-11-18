3e3b34dffde8915df9f6811a9780e48bab52c615
Payten Giles <payten.giles@gmail.com>
2019-11-18 14:37:07 +1100

    Piv#169637950 ensure certainty is mapped for mandate and function

--- backend/index_feed/function_mapper.rb
+++ backend/index_feed/function_mapper.rb
@@ -16,7 +16,7 @@ class FunctionMapper < AbstractMapper
     whitelisted['title'] = json.title
     whitelisted['source'] = json.source
     whitelisted['note'] = json.note
-    whitelisted['date'] = parse_date(json.date)
+    whitelisted['date'] = parse_dates([json.date].compact).first
     whitelisted['function_relationships'] = parse_series_system_rlshps(json.series_system_function_relationships, 'series_system_function_function_containment_relationship')
     whitelisted['agent_relationships'] = parse_series_system_rlshps(json.series_system_agent_relationships, 'series_system_agent_function_administers_relationship')
     whitelisted['mandate_relationships'] = parse_series_system_rlshps(json.series_system_mandate_relationships, ['series_system_function_mandate_creation_relationship', 'series_system_function_mandate_association_relationship', 'series_system_function_mandate_abolition_relationship'])
@@ -26,17 +26,5 @@ class FunctionMapper < AbstractMapper
 
   def parse_description(jsonmodel)
     jsonmodel['note']
-  end
-
-  def parse_date(date)
-    return if date.nil?
-
-    {
-      'begin' => date['begin'],
-      'end' => date['end'],
-      'certainty' => date['certainty'],
-      'certainty_end' => date['certainty_end'],
-      'date_notes' => date['date_notes'],
-    }
   end
 end
--- backend/index_feed/mandate_mapper.rb
+++ backend/index_feed/mandate_mapper.rb
@@ -16,7 +16,7 @@ class MandateMapper < AbstractMapper
     whitelisted['title'] = json.title
     whitelisted['mandate_type'] = json.mandate_type
     whitelisted['note'] = json.note
-    whitelisted['date'] = parse_date(json.date)
+    whitelisted['date'] = parse_dates([json.date].compact).first
 
     whitelisted['function_relationships'] = parse_series_system_rlshps(json.series_system_function_relationships, ['series_system_function_mandate_creation_relationship', 'series_system_function_mandate_association_relationship', 'series_system_function_mandate_abolition_relationship'])
     whitelisted['agent_relationships'] = parse_series_system_rlshps(json.series_system_agent_relationships, ['series_system_agent_mandate_creation_relationship', 'series_system_agent_mandate_administers_relationship'])
@@ -27,16 +27,6 @@ class MandateMapper < AbstractMapper
 
   def parse_description(jsonmodel)
     jsonmodel['note']
-  end
-
-  def parse_date(date)
-    return if date.nil?
-
-    {
-      'begin' => date['begin'],
-      'end' => date['end'],
-      'date_notes' => date['date_notes'],
-    }
   end
 
 end
