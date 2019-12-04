a7957b8b41897e9a792131bf7327e40ae4eac005
jambun <jambun@gmail.com>
2019-12-04 09:38:16 -0500

    Fix locale ref for rrr statuses

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -4,7 +4,7 @@ ArchivesSpace::Application.extend_aspace_routes(File.join(File.dirname(__FILE__)
 Rails.application.config.after_initialize do
   Plugins.add_search_facets(:reading_room_request, "rrr_status_u_ssort")
   Plugins.add_facet_group_i18n("rrr_status_u_ssort",
-                               proc {|facet| "reading_room_requests.statuses.#{facet.downcase}" })
+                               proc {|facet| "reading_room_request.statuses.#{facet.downcase}" })
 
   # Eager load all JSON schemas
   Dir.glob(File.join(File.dirname(__FILE__), "..", "schemas", "*.rb")).each do |schema|
